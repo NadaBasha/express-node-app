8e71905acd67a92794662df0febb15bc0995dcf0
NadaBasha <Nada.basha@itworx.com>
2018-12-07 16:26:15 +0200

    updates in buildspec

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req,res)=>res.send('Hello World  after updating the source code'))
+app.get('/', (req,res)=>res.send('Hello World'))
 
 app.listen(port, ()=>console.log(`Example app listening on port ${port}!`))
